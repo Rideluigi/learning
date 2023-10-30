9cac6933c132f71c0aa29a3913f369f06b1b8b24
Rideluigi <luisefrengs@gmail.com>
2023-10-30 09:40:23 -0600

    Looking at numbers and operators

--- HelloNewWorld/Program.cs
+++ HelloNewWorld/Program.cs
@@ -1,8 +1,14 @@
-﻿using System;
+﻿
+using System;
 class HelloWorld
 {
     static void Main(string[] args)
-    Console.WriteLine("Hello, World!");
+    {
+        Console.WriteLine("Hello, World! first time ever using C#");
+        int i = 10;
+        double o = 3.6;
+        Console.WriteLine(i-o);
+
+    }
+    
 }
-
-
